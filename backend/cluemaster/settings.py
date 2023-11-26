0bbaa144a5328e8c8edbc02b1f570838c43b8947
FurahaAswan <furaha.aswan@gmail.com>
2023-11-26 06:47:31 -0600

    Update settings.py

--- backend/cluemaster/settings.py
+++ backend/cluemaster/settings.py
@@ -36,8 +36,6 @@ ALLOWED_HOSTS = json.loads(os.environ.get('ALLOWED_HOSTS'))
 
 CORS_ALLOWED_ORIGINS = json.loads(os.environ.get('ALLOWED_ORIGINS'))
 
-CORS_ALLOW_CREDENTIALS = True
-
 
 
 # Application definition
